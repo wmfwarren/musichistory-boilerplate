aae79f2240e48fd3630b4d2985bdc293bccbd1d5
Marshall Friskics-Warren <wmfwarren@gmail.com>
2016-06-03 14:33:52 -0500

    Added import functionality for one list, need scroll bar

--- js/songs.js
+++ js/songs.js
@@ -1,40 +1,34 @@
-var songs = [];
-var songDisplayRegion = document.getElementsByClassName("songDisplay");
-var currentSong = null;
+var importedSongs = null;
+var requestSongData = new XMLHttpRequest();
+var songPrintZone = document.getElementById("songDisplay");
 
-songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
-songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
-songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+function printSongInfo() {
 
-function purgeSpecialChars () {
-	for (let i = 0; i < songs.length; i++) { //this loop iterates through the song strings
-		currentSong = songs[i];
+	//console.log("function triggered");
 
-		for(let j = 0; j < currentSong.length; j++){ //this loop iterates through chars in songs
-			let currentChar = currentSong[j];
-			let currentCharCode = currentSong.charCodeAt(j); //current char unicode value
+	for(let i = 0; i < importedSongs.songs.length; i++){
+		var currentSong = importedSongs.songs[i];
 
-			if (currentChar === '>') {
-				currentSong = currentSong.replace(currentChar, '-'); // replace the '>'
-			}
+		//console.log("Are I can Print song NAU?!/!!1");
 
-			if (((currentCharCode <= 47 && currentCharCode >= 33) || currentCharCode === 64) && currentCharCode !== 45) { //if unicode value is a special char...
-				currentSong = currentSong.replace(currentChar, ''); //...cut it out
-			}
-			songs[i] = currentSong;
-		}
-		//console.log("Song String ", i, ' ', currentSong);
+		songPrintZone.innerHTML += `
+			<h2 class="songName">${currentSong.title}</h2>
+			<ul>
+				<li>${currentSong.album}</li>
+				<li>${currentSong.artist}</li>
+			</ul> 
+		`;
 	}
 }
 
-songs.forEach(purgeSpecialChars);
+function readInputSongFile() {
+	importedSongs = JSON.parse(this.responseText);
+	//console.log("songs are", importedSongs);
 
-for (let i = 0; i < songs.length; i++) {
-	var currentSongToPrint = songs[i];
-	songDisplayRegion[0].innerHTML += `<p>${currentSongToPrint}</p>`;
+	printSongInfo();
 }
 
+requestSongData.addEventListener("load", readInputSongFile);
 
-
+requestSongData.open("GET", "../json/songs.json");
+requestSongData.send();
