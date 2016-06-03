10bbce7b0de81eaf620cf7eda2d252b207bb2e37
Marshall Friskics-Warren <wmfwarren@gmail.com>
2016-06-03 14:57:20 -0500

    added functionality to list more songs after the first five

--- js/songs.js
+++ js/songs.js
@@ -1,6 +1,9 @@
 var importedSongs = null;
+var moreImportedSongs = null;
 var requestSongData = new XMLHttpRequest();
+var requestMoreSongData = new XMLHttpRequest();
 var songPrintZone = document.getElementById("songDisplay");
+var moreButton = document.getElementById("moreButton");
 
 function printSongInfo() {
 
@@ -21,14 +24,35 @@ function printSongInfo() {
 	}
 }
 
+function addMoreSongInfo() {
+	for(let i = 0; i < moreImportedSongs.songs.length; i++){
+		var currentSong = moreImportedSongs.songs[i];
+
+		//console.log("Are I can Print song NAU?!/!!1");
+
+		songPrintZone.innerHTML += `
+			<h2 class="songName">${currentSong.title}</h2>
+			<ul>
+				<li>${currentSong.album}</li>
+				<li>${currentSong.artist}</li>
+			</ul> 
+		`;
+	}
+}
+
 function readInputSongFile() {
-	importedSongs = JSON.parse(this.responseText);
-	//console.log("songs are", importedSongs);
+	importedSongs = JSON.parse(requestSongData.responseText);
+	moreImportedSongs = JSON.parse(requestMoreSongData.responseText);
+	console.log("songs are", moreImportedSongs);
 
 	printSongInfo();
 }
 
 requestSongData.addEventListener("load", readInputSongFile);
+requestMoreSongData.addEventListener("load", readInputSongFile);
+moreButton.addEventListener("click", addMoreSongInfo);
 
 requestSongData.open("GET", "../json/songs.json");
+requestMoreSongData.open("GET", "../json/songs2.json");
 requestSongData.send();
+requestMoreSongData.send();
